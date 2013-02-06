4a3103177379c498f781c622287046f8f5fbf61d
Carlos Oporto <carlosoporto@gmail.com>
2013-02-06 01:12:51 -0500

    add "menu to uppercase and remove search"

--- wp-content/themes/workeet/header.php
+++ wp-content/themes/workeet/header.php
@@ -79,13 +79,6 @@
 					</ul> <!-- end ul#nav -->
 				<?php }
 				else echo($primaryNav); ?>
-						
-				<div id="search-form">
-					<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-						<input type="text" value="<?php esc_attr_e('Buscar...', 'Workeet'); ?>" name="s" id="searchinput" />
-						<input type="image" src="<?php echo get_template_directory_uri(); ?>/images/search_btn.png" id="searchsubmit" />
-					</form>
-				</div> <!-- end #search-form -->
 				
 				<?php do_action('et_header_top'); ?>
 			</div> <!-- end .container -->
